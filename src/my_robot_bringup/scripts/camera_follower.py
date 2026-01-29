b6dd6bdfa2da4d21f534515aa14c22a14d9d0b5b
dimmyb1 <demetriabugeja3@gmail.com>
2026-01-29 17:12:31 +0100

    PID found

--- src/my_robot_bringup/scripts/camera_follower.py
+++ src/my_robot_bringup/scripts/camera_follower.py
@@ -43,9 +43,9 @@ class CameraFollower(Node):
         self.odom_ready = False
         self.cardinals_initialized = False # New flag to set cardinals once
 
-        self.kp = 0.6
+        self.kp = 0.4706
         self.ki = 0.0
-        self.kd = 0.0
+        self.kd = 0.0471
 
         # offset from center
         self.line_error = 0.0
