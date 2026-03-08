da4ec566ed5c0c72dc71e2c5a2923deb21cf796b
dimmyb1 <demetriabugeja3@gmail.com>
2026-03-08 21:41:20 +0100

    x

--- src/my_robot_bringup/scripts/line_follower.py
+++ src/my_robot_bringup/scripts/line_follower.py
@@ -41,11 +41,8 @@ class line_follower():
         self.leftOneEighty = 3200
         # rightOneEighty = 3000 # Not tested (not used)
         # fullRot = realDelay*39 # Not tested (not used)
-        self.ir_M_detected = False
-        self.ir_L_detected = False
-        self.ir_R_detected = False
 
-        
+
         # Subscriptions
         self.ir_L_sub = self.create_subscription(
             Image,
@@ -77,21 +74,21 @@ class line_follower():
         img = np.frombuffer(msg.data, np.uint8).reshape(h, w, 3)
 
         # Check if robot CENTRE is over magenta - intersection confirmation
-        self.ir_M_detected = self.detect_black(img)
+        self.colours[1] = self.detect_black(img)
         
     def ir_L_callback(self, msg):
         h, w = msg.height, msg.width
         img = np.frombuffer(msg.data, np.uint8).reshape(h, w, 3)
 
         # Check if robot CENTRE is over magenta - intersection confirmation
-        self.ir_L_detected = self.detect_black(img)
+        self.colours[0] = self.detect_black(img)
 
     def ir_R_callback(self, msg):
         h, w = msg.height, msg.width
         img = np.frombuffer(msg.data, np.uint8).reshape(h, w, 3)
 
         # Check if robot CENTRE is over magenta - intersection confirmation
-        self.ir_R_detected = self.detect_black(img)
+        self.colours[2] = self.detect_black(img)
 
     # --------------------------
     # Motor Control Functions
@@ -136,15 +133,10 @@ class line_follower():
         # While the angle is less then the expected turn
         while (elapsed < totalDuration):
             # Continue turning right
-            turnRight(stepDelay)
-
-            # Read sensor again
-            colours[0] = AppITR20001.DeviceDriverSet_ITR20001_getAnaloguexxx_L()
-            colours[1] = AppITR20001.DeviceDriverSet_ITR20001_getAnaloguexxx_M()
-            colours[2] = AppITR20001.DeviceDriverSet_ITR20001_getAnaloguexxx_R()
+            self.turnRight(stepDelay)
 
             # Check if there is a black line in one of the sensors if so return to continue executing
-            if (blackMin < colours[1] < blackMax) or  (blackMin < colours[0] < blackMax) or (blackMin < colours[2] < blackMax) :
+            if (self.blackMin < self.colours[1] < self.blackMax) or  (self.blackMin < self.colours[0] < self.blackMax) or (self.blackMin < self.colours[2] < self.blackMax) :
                 self.get_logger().info("Line found during right turn!")
                 return True
             
@@ -160,15 +152,10 @@ class line_follower():
         # While the angle is less then the expected turn
         while (elapsed < totalDuration):
             # Continue turning left
-            turnLeft(stepDelay)
-
-            # Read sensor again
-            colours[0] = AppITR20001.DeviceDriverSet_ITR20001_getAnaloguexxx_L()
-            colours[1] = AppITR20001.DeviceDriverSet_ITR20001_getAnaloguexxx_M()
-            colours[2] = AppITR20001.DeviceDriverSet_ITR20001_getAnaloguexxx_R()
+            self.turnLeft(stepDelay)
 
             # Check if there is a black line in one of the sensors if so return to continue executing
-            if (blackMin < colours[1] < blackMax) or (blackMin < colours[0] < blackMax) or (blackMin < colours[2] < blackMax):
+            if (self.blackMin < self.colours[1] < self.blackMax) or (self.blackMin < self.colours[0] < self.blackMax) or (self.blackMin < self.colours[2] < self.blackMax):
                 self.get_logger().info("Line found during left turn!")
                 return True
             
@@ -178,25 +165,33 @@ class line_follower():
         return False
         
 
-
-    def setup(self) :
-        Serial.begin(9600)
-        #AppITR20001.DeviceDriverSet_ITR20001_Init()
-
-
-    def loop(self) :
-        self.get_logger().info("ITR20001_getAnaloguexxx_L=")
-        colours[0] = AppITR20001.DeviceDriverSet_ITR20001_getAnaloguexxx_L()
-        self.get_logger().info(colours[0])
-        self.get_logger().info("ITR20001_getAnaloguexxx_M=")
-        colours[1] = AppITR20001.DeviceDriverSet_ITR20001_getAnaloguexxx_M()
-        self.get_logger().info(colours[1])
-        self.get_logger().info("ITR20001_getAnaloguexxx_R=")
-        colours[2] = AppITR20001.DeviceDriverSet_ITR20001_getAnaloguexxx_R()
-        self.get_logger().info(colours[2])
+    def loop(self):
+        self.get_logger().info(f"ITR20001_getAnaloguexxx_L={self.colours[0]}")
+        self.get_logger().info(f"ITR20001_getAnaloguexxx_M={self.colours[1]}")
+        self.get_logger().info(f"ITR20001_getAnaloguexxx_R={self.colours[2]}")
 
         # Call the follow line function
-        followLine()
+        self.followLine()
+
+    
+    def main() :
+        rclpy.init()
+        node = line_follower()
+        
+        try:
+            rclpy.spin(node)
+        except KeyboardInterrupt:
+            pass
+
+        # Stop the robot
+        stop_cmd = Twist()
+        node.publisher.publish(stop_cmd)
+        node.get_logger().info('Shutting down - Robot stopped')
+
+        node.destroy_node()
+        rclpy.shutdown()
+        
+    
 
 
     def moveForwardWhileOnTrack(self) :
