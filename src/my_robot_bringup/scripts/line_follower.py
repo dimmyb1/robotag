665d5a36588a35f6a26666a58c38459608662ac3
dimmyb1 <demetriabugeja3@gmail.com>
2026-04-03 00:42:17 +0200

    east

--- src/my_robot_bringup/scripts/line_follower.py
+++ src/my_robot_bringup/scripts/line_follower.py
@@ -412,31 +412,31 @@ class line_follower(Node):
                         if(iy >= (y - math.ceil(diffY / 2))):
                             #we need to do the diffY/2 ones at this ix value
                             for ix in range(math.ceil(diffX/2)):
-                                if(x - ix > 0):
-                                    servoCells.append(Cell(x - ix, iy))
+                                if(x + ix < 5):
+                                    servoCells.append(Cell(x + ix, iy))
                         else:
                             for ix in range(diffX):
-                                if(x - ix > 0):
-                                    servoCells.append(Cell(x - ix, iy))
+                                if(x + ix < 5):
+                                    servoCells.append(Cell(x + ix, iy))
 
                         iy-=1
                             
                     #now make the smaller triangle
-                    diffY = math.ceil(diffX * math.tan(lesserServo))
+                    diffX = math.ceil(diffX * math.tan(lesserServo))
 
-                    ix = x - 1
-                    while(ix>-1):
-                        if(ix > (x - math.ceil(diffX / 2))):
+                    iy = y - 1
+                    while(iy>-1):
+                        if(iy >= (y - math.ceil(diffY / 2))):
                             #we need to do the diffY/2 ones at this ix value
-                            for iy in range(math.ceil(diffY/2)):
-                                if(y - iy > 0):
-                                    servoCells.remove(Cell(ix, y-iy))
+                            for ix in range(math.ceil(diffX/2)):
+                                if(x + ix < 5):
+                                    servoCells.remove(Cell(x + ix, iy))
                         else:
-                            for iy in range(diffY):
-                                if(y - iy > 0):
-                                    servoCells.remove(Cell(ix, y-iy))
+                            for ix in range(diffX):
+                                if(x + ix < 5):
+                                    servoCells.remove(Cell(x + ix, iy))
 
-                        ix-=1
+                        iy-=1
 
                 #elif west
 
