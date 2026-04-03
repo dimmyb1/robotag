2409b2c23bbae850e9a8603a41a509e137af54ef
dimmyb1 <demetriabugeja3@gmail.com>
2026-04-03 19:27:28 +0200

    x

--- src/my_robot_bringup/scripts/line_follower.py
+++ src/my_robot_bringup/scripts/line_follower.py
@@ -410,14 +410,14 @@ class line_follower(Node):
 
                     iy = y - 1
                     while(iy>-1):
-                        if(iy >= (y - math.ceil(diffY / 2))):
+                        if((iy >= (y - math.ceil(diffY / 2))) and y%2==0) or ((iy > (y - math.ceil(diffY / 2))) and y%2!=0):
                             #we need to do the diffY/2 ones at this ix value
                             for ix in range(math.ceil(diffX/2)):
-                                if(x + ix < 5):
+                                if(x + ix < 5) and (x + ix > -1):
                                     servoCells.append(Cell(x + ix, iy))
                         else:
                             for ix in range(diffX):
-                                if(x + ix < 5):
+                                if(x + ix < 5) and (x + ix > -1):
                                     servoCells.append(Cell(x + ix, iy))
 
                         iy-=1
@@ -430,11 +430,11 @@ class line_follower(Node):
                         if(iy >= (y - math.ceil(diffY / 2))):
                             #we need to do the diffY/2 ones at this ix value
                             for ix in range(math.ceil(diffX/2)):
-                                if(x + ix < 5):
+                                if(x + ix < 5) and (x + ix > -1):
                                     servoCells.remove(Cell(x + ix, iy))
                         else:
                             for ix in range(diffX):
-                                if(x + ix < 5):
+                                if(x + ix < 5) and (x + ix > -1):
                                     servoCells.remove(Cell(x + ix, iy))
 
                         iy-=1
