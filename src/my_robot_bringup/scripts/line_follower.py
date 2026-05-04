fcecbc71482354c6af5aa900df56fb24ad3788c6
dimmyb1 <demetriabugeja3@gmail.com>
2026-05-04 13:36:48 +0200

    -#

--- src/my_robot_bringup/scripts/line_follower.py
+++ src/my_robot_bringup/scripts/line_follower.py
@@ -2761,11 +2761,6 @@ class line_follower(Node):
             self.imu_turning = True
             self.get_logger().info(f"Turning to face {self.imu_target}")
 
-            # if ((self.facing < self.imu_target) or (self.facing == 3 and self.imu_target == 0)) and ((self.imu_target != 3 and self.facing != 0) or (self.facing == 3 and self.imu_target == 0)):
-            #     self.turnRight(0)
-            # else:
-            #     self.turnLeft(0)
-
             if (self.facing == 0 and self.imu_target == 3) or (self.facing == 1 and self.imu_target == 0) or (self.facing == 2 and self.imu_target == 1) or (self.facing == 3 and self.imu_target == 2):
                 self.turnLeft(0)
             else:
