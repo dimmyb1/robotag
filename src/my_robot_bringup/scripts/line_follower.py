cd8d64e30f6293c1adc05c1c1c232751eab71174
dimmyb1 <demetriabugeja3@gmail.com>
2026-05-05 17:23:07 +0200

    tuning

--- src/my_robot_bringup/scripts/line_follower.py
+++ src/my_robot_bringup/scripts/line_follower.py
@@ -532,7 +532,7 @@ class line_follower(Node):
     #--------------------
 
     def crawlBack(self):
-        self.start_motion(linear=-0.25, duration_ms=3000)
+        self.start_motion(linear=-0.25, duration_ms=750)
 
     #---------------------
     # Searching for Line
