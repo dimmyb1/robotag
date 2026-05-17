150f24af9b88ad5e1102766b8987c9a23f2f0cbd
dimmyb1 <demetriabugeja3@gmail.com>
2026-05-17 23:44:04 +0200

    tuning

--- src/my_robot_bringup/scripts/line_follower.py
+++ src/my_robot_bringup/scripts/line_follower.py
@@ -630,7 +630,7 @@ class line_follower(Node):
         self.start_motion(linear=-0.35, duration_ms=300) #3000ms was too much, 750ms was too much, 300ms was too little, tried 500ms, went down to 300 again and raised pwr
 
     def joltBack(self):
-        self.start_motion(linear=-0.35, duration_ms=750) #was 300, too little, was 700, ok but a bit sh ort, 850 too much, 750
+        self.start_motion(linear=-0.35, duration_ms=500) #was 300, too little, was 700, ok but a bit sh ort, 850 too much, 750 too much, 500
 
     def crawlForward(self):
         self.stopMov()
