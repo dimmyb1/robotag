b744b13cd97d50d3fcae7d22a38c5307f0d32663
dimmyb1 <demetriabugeja3@gmail.com>
2026-05-19 13:52:00 +0200

    x?

--- src/my_robot_bringup/scripts/line_follower.py
+++ src/my_robot_bringup/scripts/line_follower.py
@@ -3136,7 +3136,7 @@ class line_follower(Node):
             self.exit_angle = float('inf')
             self.ultrasonic_distance = float('inf')
 
-        if self.ultrasonic_distance <= self.CAPTURE_MAX and not (self.resetBehaviour or self.movBackCosTag or self.movThenPause or self.paused):
+        if self.ultrasonic_distance <= self.CAPTURE_MAX and not (self.imu_turning or self.movBackCosTag or self.movThenPause or self.paused):
             self.stopMov()
             self.safetyStop = True
         elif self.ultrasonic_distance > self.CAPTURE_MAX and self.safetyStop:
