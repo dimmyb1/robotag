bd843706558888dc69f44e25ca653ad229739910
dimmyb1 <demetriabugeja3@gmail.com>
2026-05-23 19:58:06 +0200

    t

--- src/my_robot_bringup/scripts/line_follower.py
+++ src/my_robot_bringup/scripts/line_follower.py
@@ -284,18 +284,18 @@ class line_follower(Node):
         self.get_logger().info("Patrol VS Avoidant :Q ")
         if robot_name == 'twix':
             self.current_node = self.H
-            self.get_logger().info("Detected robot: twix. Starting at Node H. Pursuing :Q ")
+            self.get_logger().info("Detected robot: twix. Starting at Node H. Evading :Q ")
             other_robot_name = 'twirl'
-            self.behaviourMode = 1
-            self.otherMode = 4
+            self.behaviourMode = 4
+            self.otherMode = 1
             self.evading = False
             self.i_patrol = 7
         elif robot_name == 'twirl':
             self.current_node = self.A
-            self.get_logger().info("Q: Detected robot: twirl. Starting at Node A. Evading :Q ")
+            self.get_logger().info("Q: Detected robot: twirl. Starting at Node A. Pursuer :Q ")
             other_robot_name = 'twix'
-            self.behaviourMode = 4
-            self.otherMode = 1
+            self.behaviourMode = 1
+            self.otherMode = 4
             self.evading = True
             self.i_patrol = 1
         else:
